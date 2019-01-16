e4dd01c1225416793eb275b5d568feb7252f9450
krsgn <43390457+krsgn@users.noreply.github.com>
2019-01-15 19:59:40 -0700

    Edit by Kristy
    program now asks for number of points in image

--- Image Coordinates/findPoints.m
+++ Image Coordinates/findPoints.m
@@ -1,4 +1,4 @@
-function [files,points] = findPoints( source,destination,extension,numberOfPoints,file)
+function [files,points] = findPoints(source,destination,extension,file)
 %The function allows you to click points in all the images in a folder and
 %save them to a file. The function arguments are:
 %   source: The path to the folder of gray scale images.
@@ -11,36 +11,39 @@ function [files,points] = findPoints( source,destination,extension,numberOfPoint
 %   be saved to a file. If you set the number of points to be 1, then you
 %   will be allowed to click for a point at an image, then after you select
 %   the point a new image appears.
-%
+
 path=strcat(source,'\*.',extension);
 files=dir(path);
 fid=fopen(file,'w');
-n=3;
-for i=1:n
+for i=1:length(files)
     path=strcat(source,'\',files(i).name);
     img=imread(path);
-    fprintf(fid,'%s\n',path);
+    
+    fprintf('%s\n',path);
+    prompt='Number of points in this image? ';
+    numberOfPoints=input(prompt);
+    
     for j=1:numberOfPoints
         imshow(img);
         pause;
         pt=ginput(1);
-        fprintf(fid,'%f\t%f\n',pt(1),pt(2));
+        
+        prompt2='Point name? ';
+        pt_name=input(prompt2);
+        
+        fprintf(fid,'%s\t%i\t%f\t%f\t\n',path,pt_name,pt(1),pt(2));
         points(i,j,1:2)=[pt(1),pt(2)];
     end
-end
-for i=1:n
-    path=strcat(source,'\',files(i).name);
-    img=imread(path);
+    
     path2=strcat(destination,'\',files(i).name);
     figure
     imshow(img);
     hold on;
     for j=1:numberOfPoints
-        plot(points(i,j,1),points(i,j,2),'Marker','o','MarkerFaceColor','red','MarkerSize',10);
+        plot(points(i,j,1),points(i,j,2),'Marker','x','MarkerFaceColor','red','MarkerSize',20);
     end
     print(path2,'-dtiff');
     close all;
 end
 fclose(fid);
 end
-
